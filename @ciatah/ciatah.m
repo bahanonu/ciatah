0b07e93429c3df8240c3c578cda08a7e6b23b9a2
Biafra Ahanonu <bahanonu@alum.mit.edu>
2021-03-21 17:43:51 -0700

    Move java.opts, exampleFxn.m, and VERSION

--- @ciatah/ciatah.m
+++ @ciatah/ciatah.m
@@ -1,7 +1,7 @@
 classdef ciatah < dynamicprops
 	% Performs analysis on calcium imaging data. Also performs analysis on behavior (response signals) compared to stimulus or other continuous signals during an imaging session.
 	% Biafra Ahanonu
-	% started: 2014.07.31
+	% started: 2014.07.31 (2020.10.25 [22:30:29])
 	% This is a re-write of my old code from controllerAnalysis. Encapsulating the functions and variables as methods and properties in a class should allow easier maintenance/flexibility.
 	% inputs
 		%
